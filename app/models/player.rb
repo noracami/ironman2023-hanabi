fc7848c8856bbefc34235d51597061918f5cf3d6
noracami <kerke.2011@gmail.com>
2023-10-08 03:30:00 +0800

    ⚗️ try OAuth to GitHub

---
+++ app/models/player.rb
@@ -0,0 +1,3 @@
+class Player < ApplicationRecord
+  authenticates_with_sorcery!
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "game_rooms#index"
+
+  post "oauth/callback" => "oauths#callback"
+  get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
+  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 end
